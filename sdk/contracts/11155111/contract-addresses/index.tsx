d362c1c23f2eb5baf06577391e28e5202dc5af03
githubotoro <yupuday.git@gmail.com>
2024-10-25 12:25:56 -0700

    feat(deployments): mainnet

--- sdk/contracts/11155111/contract-addresses/index.tsx
+++ sdk/contracts/11155111/contract-addresses/index.tsx
@@ -2,9 +2,9 @@ import { Address } from "abitype";
 
 export const ContractAddresses = {
   WrappedVault: "0x0000000000000000000000000000000000000000",
-  WrappedVaultFactory: "0x5c12ae3b1e9e9f21955bbee9aaf6b483991e2a23",
-  PointsFactory: "0x07bfeea32bd444b6c955aabf0f2fb71eb4185975",
-  RecipeMarketHub: "0x56c69f3cc9bc357d17d886bea615c51faab36dde",
-  VaultMarketHub: "0x6545dcf7ac7d1275bda590f9bba69a9c708f3f89",
-  WeirollWallet: "0xe08fff58b94b2c6101d073adacadeeb188847e97",
+  WrappedVaultFactory: "0xded25e49bb9ef6172c73c2d50667779eef503937",
+  PointsFactory: "0xeaa168a383105ee15edd1d3816f88f9289710079",
+  RecipeMarketHub: "0xbf29db24d0b513deecf3915720abda5b94c46739",
+  VaultMarketHub: "0xfc94ce989c77c6b388e302cd4ff31ca1faae8783",
+  WeirollWallet: "0x593f45d6de842ad9ebaf2de4fd6e00aa2a41f436",
 } as Record<string, Address>;
--- sdk/contracts/index.tsx
+++ sdk/contracts/index.tsx
@@ -1,7 +1,9 @@
 import { Contracts as Contracts_11155111 } from "./11155111";
+import { Contracts as Contracts_1 } from "./1";
 
 export const ContractMap = {
   11155111: Contracts_11155111,
+  1: Contracts_1,
 };
 
 export * from "./utils";
