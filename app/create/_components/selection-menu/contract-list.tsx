ea2fd6a1ec8dbe4144aad725a8c65a719a3cc4df
Naimish Lukhi <lukhinaimish@gmail.com>
2024-10-30 23:12:43 -0400

    feat: pin contract

--- app/create/_components/selection-menu/contract-list.tsx
+++ app/create/_components/selection-menu/contract-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
 import {
@@ -19,6 +19,8 @@ import { MarketBuilderFormSchema } from "../market-builder-form";
 import { useFunctionForm } from "@/store/use-function-form";
 import { ContractRow } from "./contract-row";
 import { PinnedContracts } from "./pinned-contracts";
+import { useContract, useSearchContracts } from "../../../../sdk/hooks";
+import { ContractMap } from "../../../../sdk/contracts";
 
 export function getStyle(style, snapshot) {
   if (!snapshot.isDragging) return {};
@@ -57,6 +59,48 @@ export const ContractList = React.forwardRef<
   const { dragData, setDragData } = useMarketBuilder();
 
   const { setIsContractAddressUpdated } = useMarketBuilderManager();
+
+  const { data: tokenContract } = useContract({
+    chain_id: marketBuilderForm.watch("chain").id,
+    contract_address: marketBuilderForm.watch("asset").contract_address,
+  });
+
+  const { data: weirollContract } = useContract({
+    chain_id: marketBuilderForm.watch("chain").id,
+    contract_address:
+      ContractMap[
+        marketBuilderForm.watch("chain").id as keyof typeof ContractMap
+      ]["WeirollWalletHelper"].address.toLowerCase(),
+  });
+
+  const pinContractList = useMemo(() => {
+    const arr = [];
+
+    if (weirollContract) {
+      arr.push({
+        ...(weirollContract as any)[0],
+        id: (weirollContract as any)[0]?.contract_id,
+      });
+    }
+
+    if (tokenContract) {
+      arr.push({
+        ...(tokenContract as any)[0],
+        id: (tokenContract as any)[0]?.contract_id,
+      });
+    }
+
+    return arr;
+  }, [tokenContract, weirollContract]);
+
+  const contractList = useMemo(() => {
+    return data.filter(
+      (contract: any) =>
+        !pinContractList.find(
+          (pinContract) => pinContract.address === contract.address
+        )
+    );
+  }, [data, pinContractList]);
 
   const resetClickedKey = () => {
     if (clickedKey !== null) {
@@ -102,13 +146,31 @@ export const ContractList = React.forwardRef<
   }, [draggingId]);
 
   return (
-    <div ref={ref} className={cn("", className)} {...props}>
-      <PinnedContracts
-        clickedKey={clickedKey}
-        setClickedKey={setClickedKey}
-        functionForm={functionForm}
-        marketBuilderForm={marketBuilderForm}
-      />
+    <div
+      ref={ref}
+      className={cn("flex h-full flex-1 flex-col", className)}
+      {...props}
+    >
+      <AnimatePresence>
+        <Droppable droppableId="pin-contract-list" isDropDisabled={true}>
+          {(droppableProvided) => (
+            <ul
+              ref={droppableProvided.innerRef}
+              {...droppableProvided.droppableProps}
+            >
+              <div className="sticky">
+                <PinnedContracts
+                  data={pinContractList}
+                  clickedKey={clickedKey}
+                  setClickedKey={setClickedKey}
+                  functionForm={functionForm}
+                  marketBuilderForm={marketBuilderForm}
+                />
+              </div>
+            </ul>
+          )}
+        </Droppable>
+      </AnimatePresence>
 
       <AnimatePresence>
         <Droppable droppableId="contract-list" isDropDisabled={true}>
@@ -116,10 +178,10 @@ export const ContractList = React.forwardRef<
             <ul
               ref={droppableProvided.innerRef}
               {...droppableProvided.droppableProps}
-              className="flex grow flex-col overflow-x-hidden overflow-y-scroll"
+              className="flex-1 overflow-x-hidden overflow-y-scroll"
             >
-              {!!data &&
-                data.map(
+              {!!contractList &&
+                contractList.map(
                   (
                     // @ts-ignore
                     contract,
@@ -139,148 +201,6 @@ export const ContractList = React.forwardRef<
                         marketBuilderForm={marketBuilderForm}
                       />
                     );
-
-                    // return (
-                    //   <Draggable
-                    //     draggableId={baseKey}
-                    //     index={0}
-                    //     key={`${baseKey}:${index}`}
-                    //     disableInteractiveElementBlocking={true}
-                    //   >
-                    //     {(provided, snapshot) => (
-                    //       <li
-                    //         onClick={() => {
-                    //           if (snapshot.isDragging) return;
-
-                    //           if (contract.contract_name) {
-                    //             functionForm.setValue(
-                    //               "contract_name",
-                    //               contract.contract_name
-                    //             );
-                    //           }
-
-                    //           if (contract.image) {
-                    //             functionForm.setValue(
-                    //               "contract_image",
-                    //               contract.image
-                    //             );
-                    //           }
-
-                    //           setClickedKey(contract.id);
-                    //         }}
-                    //         tabIndex={0}
-                    //         key={`edit-selector:item:${contract.id}`}
-                    //         className="body-2 relative flex h-16 w-full shrink-0 cursor-pointer transition-all duration-200 ease-in-out hover:bg-white"
-                    //       >
-                    //         {`contract-list:${clickedKey}` === baseKey && (
-                    //           <motion.div
-                    //             initial={{ x: "-100%" }}
-                    //             animate={{ x: "100%" }}
-                    //             transition={{
-                    //               duration: 0.4,
-                    //               ease: "easeOut",
-                    //             }}
-                    //             className="absolute z-50 flex h-full w-full flex-col place-content-center items-center bg-success"
-                    //           ></motion.div>
-                    //         )}
-
-                    //         <div
-                    //           className={cn(
-                    //             "pointer-events-none absolute inset-0 z-20 w-full px-2 transition-all duration-500 ease-in-out"
-                    //           )}
-                    //         ></div>
-
-                    //         <motion.div
-                    //           initial={{ opacity: 1 }}
-                    //           animate={{ opacity: 1, filter: "blur(0)" }}
-                    //           exit={{ opacity: 0, filter: "blur(2px)" }}
-                    //           transition={{
-                    //             duration: 0.2,
-                    //             ease: "easeInOut",
-                    //             type: "spring",
-                    //           }}
-                    //           ref={provided.innerRef}
-                    //           {...provided.draggableProps}
-                    //           style={getStyle(
-                    //             provided.draggableProps.style,
-                    //             snapshot
-                    //           )}
-                    //           className={cn(
-                    //             "absolute z-10 flex h-16 w-full flex-row items-center space-x-2 px-2",
-                    //             snapshot.isDragging &&
-                    //               "z-50 cursor-grabbing rounded-xl border border-divider bg-white shadow-md"
-                    //           )}
-                    //         >
-                    //           <div
-                    //             {...provided.dragHandleProps}
-                    //             className="flex h-16 w-3 shrink-0 cursor-grab flex-col place-content-center items-center"
-                    //           >
-                    //             <GripVerticalIcon className="h-5 w-5 fill-tertiary text-tertiary" />
-                    //           </div>
-
-                    //           <div
-                    //             className={cn(
-                    //               "flex w-full grow flex-col overflow-hidden",
-                    //               "pl-1"
-                    //             )}
-                    //           >
-                    //             <div className="flex h-5 max-w-[90%] grow flex-row items-center ">
-                    //               <div className="overflow-hidden truncate text-ellipsis text-base">
-                    //                 <span className="leading-5 text-black">
-                    //                   {contract.contract_name}
-                    //                 </span>
-                    //               </div>
-
-                    //               {/**
-                    //                * Tag to verify that contract was added by Royco
-                    //                *
-                    //                * @notice Currently removed
-                    //                */}
-                    //               {contract.is_whitelisted === true && (
-                    //                 <div className="ml-1 flex h-5 w-5 shrink-0 flex-col place-content-center items-center drop-shadow-sm">
-                    //                   <svg
-                    //                     xmlns="http://www.w3.org/2000/svg"
-                    //                     width="24"
-                    //                     height="24"
-                    //                     viewBox="0 0 24 24"
-                    //                     fill="none"
-                    //                     stroke="currentColor"
-                    //                     strokeWidth="2"
-                    //                     strokeLinecap="round"
-                    //                     strokeLinejoin="round"
-                    //                     className="lucide lucide-badge-check h-4 w-4 fill-success text-success"
-                    //                   >
-                    //                     <path
-                    //                       className=""
-                    //                       d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"
-                    //                     />
-                    //                     <path
-                    //                       className="stroke-white"
-                    //                       d="m9 12 2 2 4-4"
-                    //                     />
-                    //                   </svg>
-                    //                 </div>
-                    //               )}
-                    //             </div>
-                    //             <div className="h-5 grow overflow-hidden truncate text-ellipsis lowercase">
-                    //               <span className="text-xs leading-5 text-tertiary">
-                    //                 {contract.address.slice(0, 4)}...
-                    //                 {contract.address.slice(-4)}
-                    //               </span>
-                    //             </div>
-                    //           </div>
-
-                    //           <div className="flex h-9 shrink-0 flex-col items-end">
-                    //             <ContractDropdown
-                    //               chainId={marketBuilderForm.watch("chain").id}
-                    //               address={contract.address as string}
-                    //             />
-                    //           </div>
-                    //         </motion.div>
-                    //       </li>
-                    //     )}
-                    //   </Draggable>
-                    // );
                   }
                 )}
 
