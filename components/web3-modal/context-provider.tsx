921a9b025e6d1b6af3fc9fc0235b657a15813d4c
Naimish Lukhi <lukhinaimish@gmail.com>
2024-11-07 00:00:30 -0500

    fix: explore web3 modal

--- components/web3-modal/context-provider.tsx
+++ components/web3-modal/context-provider.tsx
@@ -6,21 +6,6 @@ import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Config, cookieToInitialState, State, WagmiProvider } from "wagmi";
 
-// Setup queryClient
-const queryClient = new QueryClient();
-
-if (!projectId) throw new Error("Project ID is not defined");
-
-// Create modal
-createWeb3Modal({
-  metadata,
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true, // Optional - false as default
-  allowUnsupportedChain: false,
-});
-
 export default function AppKitProvider({
   children,
   cookies,
@@ -28,6 +13,21 @@ export default function AppKitProvider({
   children: ReactNode;
   cookies: string | null;
 }) {
+  // Setup queryClient
+  const queryClient = new QueryClient();
+
+  if (!projectId) throw new Error("Project ID is not defined");
+
+  // Create modal
+  createWeb3Modal({
+    metadata,
+    wagmiConfig: config,
+    projectId,
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    enableOnramp: true, // Optional - false as default
+    allowUnsupportedChain: false,
+  });
+
   const initialState = cookieToInitialState(config as Config, cookies);
 
   return (
