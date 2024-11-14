e14dea472c8399396131771384b749e89dc34b88
githubotoro <yupuday.git@gmail.com>
2024-11-14 11:45:46 +0700

    feat(contracts): update

--- sdk/contracts/8453/contract-addresses/index.tsx
+++ sdk/contracts/8453/contract-addresses/index.tsx
@@ -5,7 +5,7 @@ export const ContractAddresses = {
   WrappedVaultFactory: "0xb316d165d01ac68d31b297f847533d671c965662",
   PointsFactory: "0x19112adbdafb465ddf0b57ecc07e68110ad09c50",
   RecipeMarketHub: "0x76953a612c256fc497bbb49ed14147f24c4feb71",
-  VaultMarketHub: "0x52341389be638a5b8083d2b70a421f9d4c87ebcd",
+  VaultMarketHub: "0xbfac50c6b2c91ab756c1e5efab699438992cc1b2",
   WeirollWallet: "0x40a1c08084671e9a799b73853e82308225309dc0",
   WeirollWalletHelper: "0x07899ac8be7462151d6515fcd4773dd9267c9911",
 } as Record<string, Address>;
