cde47b3c672db2eab54c34bfe6f9692bebe9a81b
githubotoro <yupuday.git@gmail.com>
2024-11-28 14:34:00 -0800

    feat(custom): cancel offer button for Matthew Graham

---
+++ app/cancel-offer/page.tsx
@@ -0,0 +1,15 @@
+import { MarketManagerStoreProvider } from "@/store";
+import { CancelOfferButton } from "./cancel-offer-button";
+import { TransactionModal } from "@/components/composables";
+
+export default function CancelOffer() {
+  return (
+    <MarketManagerStoreProvider>
+      <div className="flex h-screen flex-col items-center justify-center">
+        <CancelOfferButton />
+
+        <TransactionModal />
+      </div>
+    </MarketManagerStoreProvider>
+  );
+}
