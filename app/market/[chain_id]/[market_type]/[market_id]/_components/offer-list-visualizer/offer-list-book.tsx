861c6af4297d98937c9a4e05d71280db66cc1aee
Naimish Lukhi <lukhinaimish@gmail.com>
2024-12-11 16:05:18 -0500

    fix: offer table

--- app/market/[chain_id]/[market_type]/[market_id]/_components/offer-list-visualizer/offer-list-book.tsx
+++ app/market/[chain_id]/[market_type]/[market_id]/_components/offer-list-visualizer/offer-list-book.tsx
@@ -16,94 +16,94 @@ export const OfferListBook = React.forwardRef<
   return (
     <div
       ref={ref}
-      className={cn("flex flex-col border-y border-divider", className)}
+      className={cn("flex flex-col border-t border-divider", className)}
       {...props}
     >
-      <div className="grid grid-cols-1 divide-y divide-divider md:grid-cols-2 md:divide-x md:divide-y-0">
-        <div className="divide-y divide-divider">
+      <div className="grid h-[500px] grid-cols-1 divide-y divide-divider md:grid-cols-2 md:divide-x md:divide-y-0">
+        <div className="flex flex-col divide-y divide-divider">
           <SecondaryLabel className="py-1 pl-5 pr-2 font-semibold text-black">
-            Incentives Offered
+            IP Offers
           </SecondaryLabel>
 
           <div className="grid w-full grid-cols-2 items-center divide-x divide-divider pl-5 pr-2 text-sm">
-            <SecondaryLabel className="py-1 pr-1 text-tertiary">
-              Offer
+            <SecondaryLabel className="py-1 pr-1 font-light text-tertiary">
+              Incentives
             </SecondaryLabel>
-            <SecondaryLabel className="py-1 pl-1 text-tertiary">
-              Ask
+            <SecondaryLabel className="py-1 pl-1 font-light text-tertiary">
+              Assets Requested
             </SecondaryLabel>
           </div>
 
-          {!!currentHighestOffers &&
-          !!currentHighestOffers.ip_offers &&
-          currentHighestOffers.ip_offers.length !== 0 ? (
-            currentHighestOffers?.ip_offers.map((offer, offerIndex) => {
-              const keyInfo = {
-                previousValue:
-                  !!previousHighestOffers &&
-                  offerIndex < previousHighestOffers.ip_offers.length
-                    ? (previousHighestOffers?.ip_offers[offerIndex]
-                        .input_token_data.token_amount ?? 0)
-                    : 0,
-                currentValue: offer.input_token_data.token_amount as number,
-              };
+          <div className="flex-1 divide-y divide-divider overflow-y-scroll">
+            {!!currentHighestOffers &&
+            !!currentHighestOffers.ip_offers &&
+            currentHighestOffers.ip_offers.length !== 0 ? (
+              currentHighestOffers?.ip_offers.map((offer, offerIndex) => {
+                const keyInfo = {
+                  previousValue:
+                    !!previousHighestOffers &&
+                    offerIndex < previousHighestOffers.ip_offers.length
+                      ? (previousHighestOffers?.ip_offers[offerIndex]
+                          .input_token_data.token_amount ?? 0)
+                      : 0,
+                  currentValue: offer.input_token_data.token_amount as number,
+                };
 
-              const valueInfo = {
-                previousValue:
-                  !!previousHighestOffers &&
-                  offerIndex < previousHighestOffers.ip_offers.length
-                    ? previousHighestOffers?.ip_offers[offerIndex]
-                        .tokens_data &&
-                      previousHighestOffers?.ip_offers[offerIndex].tokens_data
-                        .length > 0
+                const valueInfo = {
+                  previousValue:
+                    !!previousHighestOffers &&
+                    offerIndex < previousHighestOffers.ip_offers.length
                       ? previousHighestOffers?.ip_offers[offerIndex]
-                          .tokens_data[0].token_amount
-                      : 0
-                    : 0,
-                currentValue:
-                  offer.tokens_data && offer.tokens_data.length > 0
-                    ? offer.tokens_data[0].token_amount
-                    : 0,
-              };
+                          .tokens_data &&
+                        previousHighestOffers?.ip_offers[offerIndex].tokens_data
+                          .length > 0
+                        ? previousHighestOffers?.ip_offers[offerIndex]
+                            .tokens_data[0].token_amount
+                        : 0
+                      : 0,
+                  currentValue:
+                    offer.tokens_data && offer.tokens_data.length > 0
+                      ? offer.tokens_data[0].token_amount
+                      : 0,
+                };
 
-              return (
-                <OfferListRow
-                  key={`offer-list-row:${offer.offer_id}`}
-                  type="ap"
-                  keyInfo={keyInfo}
-                  valueInfo={valueInfo}
-                  offer={offer}
-                  className="pl-5 pr-2"
-                />
-              );
-            })
-          ) : (
-            <AlertIndicator>No offers yet</AlertIndicator>
-          )}
+                return (
+                  <OfferListRow
+                    key={`offer-list-row:${offer.offer_id}`}
+                    type="ip"
+                    keyInfo={keyInfo}
+                    valueInfo={valueInfo}
+                    offer={offer}
+                    className="pl-5 pr-2"
+                  />
+                );
+              })
+            ) : (
+              <AlertIndicator>No offers yet</AlertIndicator>
+            )}
+            <div></div>
+          </div>
         </div>
 
-        <div>
-          <div className="divide-y divide-divider">
-            <SecondaryLabel className="py-1 pl-5 pr-2 font-semibold text-black md:pl-2 md:pr-5">
-              Assets Offered
+        <div className="flex flex-col divide-y divide-divider overflow-y-scroll">
+          <SecondaryLabel className="py-1 pl-5 pr-2 font-semibold text-black md:pl-2 md:pr-5">
+            AP Offers
+          </SecondaryLabel>
+
+          <div className="grid w-full grid-cols-2 items-center divide-x divide-divider pl-5 pr-2 text-sm md:pl-2 md:pr-5">
+            <SecondaryLabel className="py-1 pr-1 font-light text-tertiary">
+              Assets
             </SecondaryLabel>
+            <SecondaryLabel className="py-1 pl-1 font-light text-tertiary">
+              Incentives Requested
+            </SecondaryLabel>
+          </div>
 
-            <div className="grid w-full grid-cols-2 items-center divide-x divide-divider pl-5 pr-2 text-sm md:pl-2 md:pr-5">
-              <SecondaryLabel className="py-1 pr-1 text-tertiary">
-                Ask
-              </SecondaryLabel>
-              <SecondaryLabel className="py-1 pl-1 text-tertiary">
-                Offer
-              </SecondaryLabel>
-            </div>
-
+          <div className="flex-1 divide-y divide-divider overflow-y-scroll">
             {!!currentHighestOffers &&
             !!currentHighestOffers.ap_offers &&
             currentHighestOffers.ap_offers.length !== 0 ? (
               currentHighestOffers?.ap_offers.map((offer, offerIndex) => {
-                const BASE_KEY = `market:offer:${offer.offer_id}-${offer.offer_side}`;
-                const INDEX_KEY = `market:offer:${offer.offer_side}:${offerIndex}`;
-
                 const keyInfo = {
                   previousValue:
                     !!previousHighestOffers &&
@@ -134,19 +134,22 @@ export const OfferListBook = React.forwardRef<
                 };
 
                 return (
-                  <OfferListRow
-                    key={`offer-list-row:${offer.offer_id}`}
-                    type="ip"
-                    keyInfo={keyInfo}
-                    valueInfo={valueInfo}
-                    offer={offer}
-                    className="pl-5 pr-2 md:pl-2 md:pr-5"
-                  />
+                  <>
+                    <OfferListRow
+                      key={`offer-list-row:${offer.offer_id}`}
+                      type="ap"
+                      keyInfo={keyInfo}
+                      valueInfo={valueInfo}
+                      offer={offer}
+                      className="pl-5 pr-2 md:pl-2 md:pr-5"
+                    />
+                  </>
                 );
               })
             ) : (
               <AlertIndicator>No offers yet</AlertIndicator>
             )}
+            <div></div>
           </div>
         </div>
       </div>
