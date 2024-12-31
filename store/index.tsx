9de1657395c2d78192ff0c3fd76af61a4c680d92
githubotoro <yupuday.git@gmail.com>
2024-12-31 10:14:13 +0530

    feat(app): subdomain fixed

--- store/index.tsx
+++ store/index.tsx
@@ -6,5 +6,4 @@ export * from "./use-market-builder-manager";
 export * from "./use-market-manager";
 export * from "./market-manager-props";
 export * from "./use-market-builder";
-export { useGlobalStates } from "./use-global-states";
-export { useSubdomain } from "./use-global-states";
+export { useGlobalStates, getSubdomain } from "./use-global-states";
