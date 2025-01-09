8254cf6052e7e25a6983f3a67fb7e4e39413d273
pixelis <zhadkov@gmail.com>
2025-01-09 20:06:49 +0100

    integrate enso

---
+++ app/market/[chain_id]/[market_type]/[market_id]/_components/market-action-form/params-step/shortucts-widget.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import EnsoWidget from "@ensofinance/shortcuts-widget";
+import { Switch } from "@/components/ui/switch";
+
+export default function ShortcutsWidget({
+  token,
+  chainId,
+}: {
+  token: string;
+  chainId?: number;
+}) {
+  const [zapInEnabled, setZapInEnabled] = React.useState(false);
+
+  return (
+    <div style={{ margin: "10px 0" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "5px",
+          justifyContent: "flex-end",
+        }}
+      >
+        <label className={"font-gt font-500"} htmlFor={"zap-in"}>
+          Get input token
+        </label>
+        <Switch
+          id={"zap-in"}
+          checked={zapInEnabled}
+          onCheckedChange={setZapInEnabled}
+        />
+      </div>
+      {zapInEnabled && (
+        <div className={"mt-1 shadow"}>
+          <EnsoWidget
+            apiKey={"b4d83fec-53ff-48b4-96b9-e7ac64de2123"}
+            obligatedTokenOut={token}
+            obligatedChainId={chainId}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
