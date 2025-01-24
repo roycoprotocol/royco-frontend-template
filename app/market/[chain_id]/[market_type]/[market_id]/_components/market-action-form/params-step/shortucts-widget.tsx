39e9f78df12aefad289085977c5493eb1c29f48f
pixelis <zhadkov@gmail.com>
2025-01-24 13:27:41 +0100

    eliminate WABTC zapin

--- app/market/[chain_id]/[market_type]/[market_id]/_components/market-action-form/params-step/shortucts-widget.tsx
+++ app/market/[chain_id]/[market_type]/[market_id]/_components/market-action-form/params-step/shortucts-widget.tsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { ButtonIcon, CaretSortIcon } from "@radix-ui/react-icons";
+import { ReactNode, useState } from "react";
+import Link from "next/link";
+import { ButtonIcon } from "@radix-ui/react-icons";
 import EnsoWidget, { SystemConfig } from "@ensofinance/shortcuts-widget";
 
 const ENSO_KEY = process.env.NEXT_PUBLIC_ENSO_API_KEY;
+
+const DISABLED_ZAPS = [
+  "0x42a094364bbdca0efac8af2cf7d6b9ec885ee554", // WABTC/WBTC uni-v2
+];
 
 // Paste styling according to the design system
 // https://www.chakra-ui.com/docs/theming/customization/overview
@@ -16,6 +21,12 @@ const themeConfig: SystemConfig = {
   },
 };
 
+const UniRedirectLink = ({ children }: { children: ReactNode }) => (
+  <Link target={"_blank"} href={"https://app.uniswap.org/positions/create/v2"}>
+    {children}
+  </Link>
+);
+
 export default function ShortcutsWidget({
   token,
   symbol,
@@ -25,26 +36,34 @@ export default function ShortcutsWidget({
   symbol: string;
   chainId?: number;
 }) {
-  const [zapInEnabled, setZapInEnabled] = React.useState(false);
+  const [zapInEnabled, setZapInEnabled] = useState(false);
+
+  const zapDisabled = DISABLED_ZAPS.includes(token);
+  const Wrapper = zapDisabled ? UniRedirectLink : "div";
 
   return (
     <div className={"mb-1"}>
       <div className={"flex w-full items-center justify-end gap-2"}>
-        <div
-          className={
-            "flex cursor-pointer items-center gap-1 text-sm font-light text-secondary underline"
-          }
-          onClick={() => setZapInEnabled((val) => !val)}
-        >
-          <ButtonIcon />
-          Get {symbol}
-        </div>
+        <Wrapper>
+          <div
+            className={
+              "flex cursor-pointer items-center gap-1 text-sm font-light text-secondary underline"
+            }
+            onClick={
+              zapDisabled ? undefined : () => setZapInEnabled((val) => !val)
+            }
+          >
+            <ButtonIcon />
+            Get {symbol}
+          </div>
+        </Wrapper>
       </div>
+
       {zapInEnabled && (
         <div className={"mt-1"}>
           <EnsoWidget
             obligateSelection
-            apiKey={ENSO_KEY ?? "b4d83fec-53ff-48b4-96b9-e7ac64de2123"}
+            apiKey={ENSO_KEY}
             tokenOut={token}
             chainId={chainId}
             themeConfig={themeConfig}
