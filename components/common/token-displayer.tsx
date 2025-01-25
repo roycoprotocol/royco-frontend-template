7a6a231010505104b9c38ec2805d06672dda9853
githubotoro <yupuday.git@gmail.com>
2025-01-25 13:13:30 -0800

    feat(app): lp token symbol

--- components/common/token-displayer.tsx
+++ components/common/token-displayer.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/tooltip";
 import { useGeneralStats } from "@/store";
 import { SparklesIcon } from "lucide-react";
+import { getSupportedToken } from "royco/constants";
 
 /**
  * @description Props for Token Displayer
@@ -82,52 +83,174 @@ const TokenDisplayer = React.forwardRef<
               <Tooltip>
                 {hover ? (
                   <TooltipTrigger asChild className={cn("cursor-pointer")}>
-                    <motion.img
-                      key={`token:${token.id}:image`}
-                      src={token.image}
-                      alt={token.symbol}
-                      className={cn(
-                        "shrink-0 rounded-full bg-z2 transition-transform",
-                        index !== 0 && "-ml-1",
-                        size === 6 && "h-6 w-6",
-                        size === 5 && "h-5 w-5",
-                        size === 4 && "h-4 w-4",
-                        imageClassName
+                    <div className="contents">
+                      {typeof token.id === "string" &&
+                      getSupportedToken(token.id).type === "lp" ? (
+                        <Fragment>
+                          <motion.img
+                            key={`token:${token.id}:image:token0`}
+                            src={
+                              getSupportedToken(
+                                getSupportedToken(token.id).token0
+                              ).image
+                            }
+                            alt={token.symbol}
+                            className={cn(
+                              "shrink-0 rounded-full bg-z2 transition-transform",
+                              index !== 0 && "-ml-1",
+                              size === 6 && "h-6 w-6",
+                              size === 5 && "h-5 w-5",
+                              size === 4 && "h-4 w-4",
+                              imageClassName
+                            )}
+                            whileHover={{ y: bounce ? -6 : 0 }}
+                            initial={{ y: 0 }}
+                            id={`token-${index}`}
+                            transition={{
+                              duration: 0.05,
+                              ease: "easeInOut",
+                              bounce: 0,
+                              type: "spring",
+                            }}
+                          />
+
+                          <motion.img
+                            key={`token:${token.id}:image:token1`}
+                            src={
+                              getSupportedToken(
+                                getSupportedToken(token.id).token1
+                              ).image
+                            }
+                            alt={token.symbol}
+                            className={cn(
+                              "shrink-0 rounded-full bg-z2 transition-transform",
+                              "-ml-2",
+                              size === 6 && "h-6 w-6",
+                              size === 5 && "h-5 w-5",
+                              size === 4 && "h-4 w-4",
+                              imageClassName
+                            )}
+                            whileHover={{ y: bounce ? -6 : 0 }}
+                            initial={{ y: 0 }}
+                            id={`token-${index}`}
+                            transition={{
+                              duration: 0.05,
+                              ease: "easeInOut",
+                              bounce: 0,
+                              type: "spring",
+                            }}
+                          />
+                        </Fragment>
+                      ) : (
+                        <motion.img
+                          key={`token:${token.id}:image`}
+                          src={token.image}
+                          alt={token.symbol}
+                          className={cn(
+                            "shrink-0 rounded-full bg-z2 transition-transform",
+                            index !== 0 && "-ml-1",
+                            size === 6 && "h-6 w-6",
+                            size === 5 && "h-5 w-5",
+                            size === 4 && "h-4 w-4",
+                            imageClassName
+                          )}
+                          whileHover={{ y: bounce ? -6 : 0 }}
+                          initial={{ y: 0 }}
+                          id={`token-${index}`}
+                          transition={{
+                            duration: 0.05,
+                            ease: "easeInOut",
+                            bounce: 0,
+                            type: "spring",
+                          }}
+                        />
                       )}
-                      whileHover={{ y: bounce ? -6 : 0 }}
-                      initial={{ y: 0 }}
-                      id={`token-${index}`}
-                      transition={{
-                        duration: 0.05,
-                        ease: "easeInOut",
-                        bounce: 0,
-                        type: "spring",
-                      }}
-                    />
+                    </div>
                   </TooltipTrigger>
                 ) : (
-                  <motion.img
-                    key={`token:${token.id}:image`}
-                    src={token.image}
-                    alt={token.symbol}
-                    className={cn(
-                      "shrink-0 rounded-full bg-z2 transition-transform",
-                      index !== 0 && "-ml-1",
-                      size === 6 && "h-6 w-6",
-                      size === 5 && "h-5 w-5",
-                      size === 4 && "h-4 w-4",
-                      imageClassName
+                  <div className="contents">
+                    {typeof token.id === "string" &&
+                    getSupportedToken(token.id).type === "lp" ? (
+                      <Fragment>
+                        <motion.img
+                          key={`token:${token.id}:image:token0`}
+                          src={
+                            getSupportedToken(
+                              getSupportedToken(token.id).token0
+                            ).image
+                          }
+                          alt={token.symbol}
+                          className={cn(
+                            "shrink-0 rounded-full bg-z2 transition-transform",
+                            index !== 0 && "-ml-1",
+                            size === 6 && "h-6 w-6",
+                            size === 5 && "h-5 w-5",
+                            size === 4 && "h-4 w-4",
+                            imageClassName
+                          )}
+                          whileHover={{ y: bounce ? -6 : 0 }}
+                          initial={{ y: 0 }}
+                          id={`token-${index}`}
+                          transition={{
+                            duration: 0.05,
+                            ease: "easeInOut",
+                            bounce: 0,
+                            type: "spring",
+                          }}
+                        />
+
+                        <motion.img
+                          key={`token:${token.id}:image:token1`}
+                          src={
+                            getSupportedToken(
+                              getSupportedToken(token.id).token1
+                            ).image
+                          }
+                          alt={token.symbol}
+                          className={cn(
+                            "shrink-0 rounded-full bg-z2 transition-transform",
+                            "-ml-2",
+                            size === 6 && "h-6 w-6",
+                            size === 5 && "h-5 w-5",
+                            size === 4 && "h-4 w-4",
+                            imageClassName
+                          )}
+                          whileHover={{ y: bounce ? -6 : 0 }}
+                          initial={{ y: 0 }}
+                          id={`token-${index}`}
+                          transition={{
+                            duration: 0.05,
+                            ease: "easeInOut",
+                            bounce: 0,
+                            type: "spring",
+                          }}
+                        />
+                      </Fragment>
+                    ) : (
+                      <motion.img
+                        key={`token:${token.id}:image`}
+                        src={token.image}
+                        alt={token.symbol}
+                        className={cn(
+                          "shrink-0 rounded-full bg-z2 transition-transform",
+                          index !== 0 && "-ml-1",
+                          size === 6 && "h-6 w-6",
+                          size === 5 && "h-5 w-5",
+                          size === 4 && "h-4 w-4",
+                          imageClassName
+                        )}
+                        whileHover={{ y: bounce ? -6 : 0 }}
+                        initial={{ y: 0 }}
+                        id={`token-${index}`}
+                        transition={{
+                          duration: 0.05,
+                          ease: "easeInOut",
+                          bounce: 0,
+                          type: "spring",
+                        }}
+                      />
                     )}
-                    whileHover={{ y: bounce ? -6 : 0 }}
-                    initial={{ y: 0 }}
-                    id={`token-${index}`}
-                    transition={{
-                      duration: 0.05,
-                      ease: "easeInOut",
-                      bounce: 0,
-                      type: "spring",
-                    }}
-                  />
+                  </div>
                 )}
 
                 {typeof window !== "undefined" &&
