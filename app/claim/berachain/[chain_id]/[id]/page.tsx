ebb6c5dda61a3b41998f2a8cbec4ab5ee1aa989d
Naimish Lukhi <lukhinaimish@gmail.com>
2025-03-06 10:09:25 -0500

    fix: add bera claim message

--- app/claim/berachain/[chain_id]/[id]/page.tsx
+++ app/claim/berachain/[chain_id]/[id]/page.tsx
@@ -1,25 +1,21 @@
 import "./local.css";
 
-import { Header } from "./_components/ui/header";
-import { Banner } from "./_components/ui/banner";
-import { ClaimCard } from "./_components/ui/claim-card";
-import { ClaimStatus } from "./_components/ui/claim-status";
 import { MarketManagerStoreProvider } from "@/store";
 import { TransactionModal } from "@/components/composables";
+import { cn } from "@/lib/utils";
 
 const Page = () => {
   return (
     <MarketManagerStoreProvider>
       <div className="flex w-full flex-col items-center px-3 py-24">
-        <Header />
+        <div className={cn("flex w-full flex-col items-center")}>
+          <h1 className="font-morion text-4xl font-bold">Berachain Claims</h1>
 
-        {/* <div className="mt-10 flex h-fit w-full max-w-2xl flex-col">
-          <Banner />
-
-          <ClaimStatus className="mt-10" />
-
-          <ClaimCard className="mt-3" />
-        </div> */}
+          <p className="mt-5 max-w-lg text-center font-gt text-base text-secondary">
+            <span className="font-bold">Undergoing Maintenance:</span> BERA
+            Claims will be back soon!
+          </p>
+        </div>
       </div>
 
       <TransactionModal />
