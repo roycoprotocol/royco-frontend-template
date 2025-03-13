b54b8b5f8837160aaf51bc7c2575f8110567e3a1
githubotoro <yupuday.git@gmail.com>
2025-03-13 16:58:35 -0700

    feat(sonic): manual execution

---
+++ app/manual/sonic/0x239b40ad70125883d948604f354ba90e1247f3ec/page.tsx
@@ -0,0 +1,15 @@
+import { MarketManagerStoreProvider } from "@/store";
+import { HardcodedButton } from "./hardcoded-button";
+import { TransactionModal } from "@/components/composables";
+
+export default function CancelOffer() {
+  return (
+    <MarketManagerStoreProvider>
+      <div className="flex h-screen flex-col items-center justify-center">
+        <HardcodedButton />
+
+        <TransactionModal />
+      </div>
+    </MarketManagerStoreProvider>
+  );
+}
