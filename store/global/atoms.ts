c7a45525bbe0b432988c78b5cd2d03da6923bae1
githubotoro <yupuday.git@gmail.com>
2025-05-21 18:01:36 -0700

    feat(global): atom udpate

--- store/global/atoms.ts
+++ store/global/atoms.ts
@@ -1,10 +1,10 @@
 import { atom } from "jotai";
 import { type CustomTokenDataElement as BaseCustomTokenDataElement } from "royco/api";
 
-// export const accountAddressAtom = atom<string | undefined>(undefined);
-export const accountAddressAtom = atom<string | undefined>(
-  "0x77777Cc68b333a2256B436D675E8D257699Aa667"
-);
+export const accountAddressAtom = atom<string | undefined>(undefined);
+// export const accountAddressAtom = atom<string | undefined>(
+//   "0x77777Cc68b333a2256B436D675E8D257699Aa667"
+// );
 
 export type CustomTokenDataElement = BaseCustomTokenDataElement & {
   allocation?: number;
