581d8454e8c8fdeb540c97a23fb25b0fdf4bca8f
githubotoro <yupuday.git@gmail.com>
2025-05-29 16:48:58 -0700

    feat(links): update

--- app/explore/components/vaults-table/vaults-table.tsx
+++ app/explore/components/vaults-table/vaults-table.tsx
@@ -14,6 +14,7 @@ import { Button } from "@/components/ui/button";
 import formatNumber from "@/utils/numbers";
 import { BtcCoinIcon } from "@/assets/icons/btc";
 import { EthCoinIcon } from "@/assets/icons/eth";
+import Link from "next/link";
 
 interface VaultsCardProps {
   data: EnrichedVault;
@@ -82,7 +83,7 @@ const VaultsCard = ({ data }: VaultsCardProps) => {
             Deposit
           </Button>
         ) : (
-          <a href={`/vault/boring/${data.chainId}/${data.vaultAddress}`}>
+          <Link href={`/vault/boring/${data.chainId}/${data.vaultAddress}`}>
             <Button
               variant="outline"
               size="sm"
@@ -91,7 +92,7 @@ const VaultsCard = ({ data }: VaultsCardProps) => {
             >
               Deposit
             </Button>
-          </a>
+          </Link>
         )}
       </div>
     </div>
