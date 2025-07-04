e77cb7925b4c9fe45930f6ae950086d654f0917a
Naimish Lukhi <lukhinaimish@gmail.com>
2025-07-04 11:52:19 -0400

    fix: api object

--- app/api/royco/index.ts
+++ app/api/royco/index.ts
@@ -1,18 +1,18 @@
 import { Api as ProductionApi } from "royco/api";
 import { Api as DevelopmentApi } from "./Api";
 
-export const api = new DevelopmentApi({
-  baseURL: "http://localhost:8000",
-  withCredentials: true,
-});
+// export const api = new DevelopmentApi({
+//   baseURL: "http://localhost:8000",
+//   withCredentials: true,
+// });
 
-// export const api =
-//   process.env.NEXT_PUBLIC_API_ENV === "development"
-//     ? new DevelopmentApi({
-//         baseURL: "http://localhost:8000",
-//         withCredentials: true,
-//       })
-//     : new ProductionApi({
-//         baseURL: "/",
-//         withCredentials: true,
-//       });
+export const api =
+  process.env.NEXT_PUBLIC_API_ENV === "development"
+    ? new DevelopmentApi({
+        baseURL: "http://localhost:8000",
+        withCredentials: true,
+      })
+    : new ProductionApi({
+        baseURL: "/",
+        withCredentials: true,
+      });
